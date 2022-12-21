f92daca68fd8b6bde4ab156cbea4fad84d49b672
Tomoki-kun <tomoki66602@gmail.com>
2022-12-20 22:41:46 -0300

    agrego componente Proyectos

--- Portfolio-ArgProg/src/app/app.module.ts
+++ Portfolio-ArgProg/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HyssComponent } from './components/hyss/hyss.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HyssComponent } from './components/hyss/hyss.component';
     AboutComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HyssComponent
+    HyssComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
