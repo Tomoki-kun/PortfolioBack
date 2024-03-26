2cfcf5723ecfc3136571faa2e16faa65295a1e55
Tomoki-kun <99996920+Tomoki-kun@users.noreply.github.com>
2024-03-26 17:19:12 -0300

    actualizaciones de version y compatibilidades de Node y componentes

---
+++ Portfolio-ArgProg/BackEnd/src/main/java/com/portfolio/Tomoki/TomokiApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Tomoki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomokiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomokiApplication.class, args);
+	}
+
+}
