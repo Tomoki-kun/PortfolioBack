af43160b861b521b247c7aeaad4ffe139a5315f4
Tomoki-kun <99996920+Tomoki-kun@users.noreply.github.com>
2024-04-07 01:26:25 -0300

    Corrigiendo CORS

--- src/main/java/com/portfolio/Tomoki/Controller/PersonaController.java
+++ src/main/java/com/portfolio/Tomoki/Controller/PersonaController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"https://portfolioargprog87.web.app","http://localhost:4200"})
 public class PersonaController {
     @Autowired IPersonaService iPersonaService;
     
