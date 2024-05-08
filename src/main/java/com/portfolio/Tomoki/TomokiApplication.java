c45fc13681441318383ed784c41a759b25f5542d
Tomoki-kun <99996920+Tomoki-kun@users.noreply.github.com>
2024-05-08 19:30:28 -0300

    probando

--- src/main/java/com/portfolio/Tomoki/TomokiApplication.java
+++ src/main/java/com/portfolio/Tomoki/TomokiApplication.java
@@ -2,8 +2,9 @@ package com.portfolio.Tomoki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class TomokiApplication {
 
 	public static void main(String[] args) {
