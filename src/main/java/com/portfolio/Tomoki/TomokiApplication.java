d8540fa71fea62d8fe45b74a2b8b22ebfa4bc4d8
Tomoki-kun <99996920+Tomoki-kun@users.noreply.github.com>
2024-05-13 13:50:15 -0300

    probando azure

--- src/main/java/com/portfolio/Tomoki/TomokiApplication.java
+++ src/main/java/com/portfolio/Tomoki/TomokiApplication.java
@@ -2,10 +2,18 @@ package com.portfolio.Tomoki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TomokiApplication {
 
+    @GetMapping("/mensaje")
+    
+    public String mensaje(){
+        return "Welcome from Java to Azure";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(TomokiApplication.class, args);
 	}
